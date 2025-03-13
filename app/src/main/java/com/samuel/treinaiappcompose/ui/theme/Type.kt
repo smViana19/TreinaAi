aac227bbe1b3cb7d050186d55de3b79592d09405
Samuu <samuelfilipevianadois@gmail.com>
2025-03-13 11:58:06 -0300

    style: add more styles to typography, colors, and strings

--- app/src/main/java/com/samuel/treinaiappcompose/ui/theme/Type.kt
+++ app/src/main/java/com/samuel/treinaiappcompose/ui/theme/Type.kt
@@ -1,13 +1,9 @@
 package com.samuel.treinaiappcompose.ui.theme
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.samuel.treinaiappcompose.R
 
@@ -37,27 +33,14 @@ val Typography = Typography(
     fontSize = 16.sp,
     letterSpacing = 0.5.sp
   ),
+  bodyMedium = TextStyle(
+    fontFamily = primaryFontFamilyRegular,
+    fontSize = 14.sp,
+    letterSpacing = 0.5.sp
+  ),
   labelMedium = TextStyle(
     fontFamily = primaryFontFamilyRegular,
     fontSize = 16.sp,
   )
-
-
-  /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
 
