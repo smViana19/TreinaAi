fe20710d2f0f4aea726a6ef3e0e0c97a47d774e9
Samuu <samuelfilipevianadois@gmail.com>
2025-03-14 17:06:35 -0300

    refactor: add signup viewmodel instance

--- app/src/main/java/com/samuel/treinaiappcompose/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/samuel/treinaiappcompose/ui/navigation/MainNavigation.kt
@@ -1,18 +1,20 @@
 package com.samuel.treinaiappcompose.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.samuel.treinaiappcompose.ui.screens.OnboardingScreen
 import com.samuel.treinaiappcompose.ui.screens.SignInScreen
 import com.samuel.treinaiappcompose.ui.screens.SignUpScreen
+import com.samuel.treinaiappcompose.ui.viewmodels.SignUpScreenViewModel
 
 @Composable
 fun MainNavigation() {
   val navController = rememberNavController()
-  //TODO: ADD INSTANCIAS VIEWMODELS
+  val signUpScreenViewModel: SignUpScreenViewModel = hiltViewModel()
+
 
   NavHost(
     navController = navController,
@@ -22,11 +24,10 @@ fun MainNavigation() {
       OnboardingScreen(navController)
     }
     composable(route = Screens.SIGNUP_SCREEN.name) {
-      SignUpScreen(navController)
+      SignUpScreen(signUpScreenViewModel,navController)
     }
     composable(route = Screens.SIGNIN_SCREEN.name) {
       SignInScreen(navController)
     }
-
   }
 }
