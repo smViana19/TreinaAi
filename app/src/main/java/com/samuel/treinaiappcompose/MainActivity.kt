49685c12863b2feb992c2ae3c9bba2bfa89ee7c3
Samuu <samuelfilipevianadois@gmail.com>
2025-03-23 19:56:00 -0300

    chore: remove unused import

--- app/src/main/java/com/samuel/treinaiappcompose/MainActivity.kt
+++ app/src/main/java/com/samuel/treinaiappcompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.samuel.treinaiappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.google.firebase.Firebase
