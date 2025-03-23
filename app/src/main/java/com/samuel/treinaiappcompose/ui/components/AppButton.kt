27fa91c77744132c954a3e1eb19f7c7191a190a5
Samuu <samuelfilipevianadois@gmail.com>
2025-03-23 19:54:14 -0300

    refactor: improve app button

--- app/src/main/java/com/samuel/treinaiappcompose/ui/components/AppButton.kt
+++ app/src/main/java/com/samuel/treinaiappcompose/ui/components/AppButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +17,7 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -29,14 +31,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.samuel.treinaiappcompose.R
 import com.samuel.treinaiappcompose.ui.theme.AppTheme
 import com.samuel.treinaiappcompose.ui.theme.Typography
 
@@ -48,8 +53,8 @@ fun AppButton(
   isLoading: Boolean = false,
   text: String? = null,
   icon: (@Composable (() -> Unit))? = null,
-  //trail icon
-  //leading icon
+  leadingIcon: (@Composable (() -> Unit))? = null,
+  trailingIcon: (@Composable (() -> Unit))? = null,
   textColor: Color = Color.Unspecified,
   enabled: Boolean = true,
   shape: Shape = ButtonDefaults.shape,
@@ -94,10 +99,15 @@ fun AppButton(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
       ) {
+        if(leadingIcon != null) {
+          leadingIcon()
+          Spacer(modifier = Modifier.width(iconSpacing))
+        }
         when {
           text != null -> {
             Text(
               text = text,
+              textAlign = TextAlign.Center,
               color = textColor,
               style = style,
             )
@@ -114,13 +124,18 @@ fun AppButton(
 
 @Composable
 fun DefaultAppButton(
+  modifier: Modifier = Modifier,
   onClick: () -> Unit,
   text: String,
+  icon: (@Composable (() -> Unit))? = null,
+  leadingIcon: (@Composable (() -> Unit))? = null,
   shape: Shape = RoundedCornerShape(12.dp),
   style: TextStyle = Typography.titleMedium
 ) {
   AppButton(
-    modifier = Modifier.fillMaxWidth().height(56.dp),
+    modifier = modifier
+      .fillMaxWidth()
+      .height(56.dp),
     onClick = {
       onClick()
     },
@@ -131,10 +146,13 @@ fun DefaultAppButton(
       disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
       disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
     ),
+    leadingIcon = leadingIcon,
+    icon = icon,
     text = text,
     style = style
   )
 }
+
 
 @Preview(showBackground = true)
 @Composable
@@ -162,6 +180,27 @@ private fun DefaultAppButtonPreview() {
       DefaultAppButton(
         onClick = {},
         text = "Create Account",
+      )
+    }
+
+  }
+}
+
+@Preview
+@Composable
+private fun AppButtonLeadingIcon() {
+  AppTheme {
+    Row (
+      modifier = Modifier
+        .width(327.dp)
+        .padding(24.dp)
+    ){
+      DefaultAppButton(
+        onClick = {},
+        leadingIcon = {
+          Icon(painter = painterResource(R.drawable.ic_lock_24), contentDescription = "")
+        },
+        text = "Add workout",
       )
     }
 
